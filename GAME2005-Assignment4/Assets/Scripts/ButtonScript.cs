f7e8a3c2445cd333ed1e99896d2a0c2389a675d9
abarnez <icecapper@hotmail.com>
2020-11-29 18:30:25 -0500

    Created Start Scene and Button

---
+++ GAME2005-Assignment4/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Button Button;
+    public string scene1;
+    void Start()
+    {
+        Button btn = Button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);    
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(scene1);        
+    }
+}

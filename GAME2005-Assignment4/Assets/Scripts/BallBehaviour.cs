b05f35206db6c7e2663a1e5b0c13d179a1d8d914
abarnez <icecapper@hotmail.com>
2020-12-02 14:38:53 -0500

    fixed bullets to fire in player dir

--- GAME2005-Assignment4/Assets/Scripts/BallBehaviour.cs
+++ GAME2005-Assignment4/Assets/Scripts/BallBehaviour.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
     public Vector3 velocity;
+    public Vector3 pDir;
+    public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
+        pDir = player.transform.forward;       
         velocity = new Vector3(0, 0, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += velocity * Time.deltaTime;
+       // pDir += velocity * Time.deltaTime;
+        transform.position += pDir * Time.deltaTime;
     }
 }

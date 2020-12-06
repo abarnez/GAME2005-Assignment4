9054e8a8f206b9537676a5d10195f0a1a9418840
abarnez <icecapper@hotmail.com>
2020-12-06 17:16:15 -0500

    un comment changes

--- GAME2005-Assignment4/Assets/Scripts/CollisionManager.cs
+++ GAME2005-Assignment4/Assets/Scripts/CollisionManager.cs
@@ -10,7 +10,7 @@ public class CollisionManager : MonoBehaviour
     public List<BallBehaviour> Spheres;
 
 
-   // public GameObject player;
+    public GameObject player;
 
     //sliders
     public Slider Velocity;
@@ -52,12 +52,12 @@ public class CollisionManager : MonoBehaviour
                 if (i != j)
                 {
                     if (AABBCheck(Cubes[i], Cubes[j]))
-                    {/*
+                    {
                         if (Cubes[i].CompareTag("Player") || Cubes[j].CompareTag("Player"))
                         {
                             Debug.Log("Player Colliding Cube");
-                           // player.transform.position  -= new Vector3(0.08f,0.0f,0.08f);
-                        } */
+                            player.transform.position  -= new Vector3(0.08f,0.0f,0.08f);
+                        } 
                         Debug.Log("Colliding Cube");
                     }
                 }

61d2a7dc635aaad6cf993818a94e57935c9181ca
Tam <tam.tahirli@gmail.com>
2020-12-15 22:11:15 -0500

    Added, and updated final provided. Added canvas with name and background, play button. Updated player movement (still needs work) Updated player colliding with box to make it move like a push. Working on new AABB with reflect.

---
+++ Assets/_Scripts/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextScene : MonoBehaviour
+{
+    public Button playBtn;
+    void Start()
+    {
+        playBtn.GetComponent<Button>().onClick.AddListener(OnClick);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
